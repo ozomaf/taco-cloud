db48a21a9617f9a254bb928f1d0487c1f3465751
azatkhaliyllin <khaliyllin56@gmail.com>
2023-03-18 18:18:48 +0400

    Chapter 3.2.3 Annotating subject area data classes

--- src/main/java/com/azatkhaliullin/tacocloud/Taco.java
+++ src/main/java/com/azatkhaliullin/tacocloud/Taco.java
@@ -3,12 +3,16 @@ package com.azatkhaliullin.tacocloud;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
+@Table
 public class Taco {
+    @Id
     private Long id;
     private Date createdAt = new Date();
 
