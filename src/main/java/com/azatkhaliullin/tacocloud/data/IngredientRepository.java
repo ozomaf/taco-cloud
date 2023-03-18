bc36dfbbd4e6ad01282e54c320f3b62e84e07a41
azatkhaliyllin <khaliyllin56@gmail.com>
2023-03-18 16:55:11 +0400

    Chapter 3.2.2 Defining repository interfaces

--- src/main/java/com/azatkhaliullin/tacocloud/data/IngredientRepository.java
+++ src/main/java/com/azatkhaliullin/tacocloud/data/IngredientRepository.java
@@ -1,12 +1,10 @@
 package com.azatkhaliullin.tacocloud.data;
 
 import com.azatkhaliullin.tacocloud.Ingredient;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
 
-public interface IngredientRepository {
+public interface IngredientRepository
+        extends CrudRepository<Ingredient, String> {
 
-    Iterable<Ingredient> findAll();
-    Optional<Ingredient> findById(String id);
-    Ingredient save(Ingredient ingredient);
 }
--- src/main/java/com/azatkhaliullin/tacocloud/data/OrderRepository.java
+++ src/main/java/com/azatkhaliullin/tacocloud/data/OrderRepository.java
@@ -1,8 +1,9 @@
 package com.azatkhaliullin.tacocloud.data;
 
 import com.azatkhaliullin.tacocloud.TacoOrder;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository {
 
-    TacoOrder save(TacoOrder order);
+public interface OrderRepository
+        extends CrudRepository<TacoOrder, Long> {
 }
